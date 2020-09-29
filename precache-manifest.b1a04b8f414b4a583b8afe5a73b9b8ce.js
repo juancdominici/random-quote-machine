33b64c70bd07a98d962244211558c6ac4d132c2d
Reshi <juancdominici@gmail.com>
2020-09-29 18:05:55 -0300

    Updates

---
+++ precache-manifest.b1a04b8f414b4a583b8afe5a73b9b8ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fafd8b9a35c1a5f4d6faba08463d9cac",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "5b853e919c0fb074a17f",
+    "url": "/random-quote-machine/static/css/main.d2669c27.chunk.css"
+  },
+  {
+    "revision": "d2f82a250ca858e8ca53",
+    "url": "/random-quote-machine/static/js/2.0f252d38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.0f252d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b853e919c0fb074a17f",
+    "url": "/random-quote-machine/static/js/main.599bfa28.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
